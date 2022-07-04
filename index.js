989c611381a3646c6a0faa5a8e8a26cb3f4d90da
Code Sandbox <hello@codesandbox.io>
2022-07-04 17:58:25 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,12 @@
+$("#features").click(function () {
+  $("#hidden").slideToggle("slow");
+  $(".hide").slideToggle("slow");
+});
+$("#company").click(function () {
+  $("#hidden1").slideToggle("slow");
+  $(".hide1").slideToggle("slow");
+});
+
+$(".menu").click(function () {
+  $(".left-list").slideToggle("slow");
+});
